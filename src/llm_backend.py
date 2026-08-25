0c455dca4e025d31eaed940a3984fe0bcf8e0c01
Wan Kamil <wan.kamil@mapof.ag>
2026-08-25 15:57:44 +1200

    bug fix

--- src/llm_backend.py
+++ src/llm_backend.py
@@ -142,7 +142,7 @@ class OpenAIBackend:
 class GroqBackend:
     """Groq (configured Groq models)."""
 
-    def __init__(self, model: str, api_key: str, base_url: str):
+    def __init__(self, model: str, api_key: str, base_url: str, json_mode: bool = True):
         self.model = model
         self.api_key = api_key
         self.base_url = base_url.rstrip("/")
